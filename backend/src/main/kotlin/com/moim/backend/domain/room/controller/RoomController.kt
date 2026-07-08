865e74a0f41941b9e5a114f370862d35b36b12fe
GwonDongHyeon21 <GwonDH21@gmail.com>
2026-07-09 00:16:43 +0900

    style(back): 엔드포인트명 수정

--- backend/src/main/kotlin/com/moim/backend/domain/room/controller/RoomController.kt
+++ backend/src/main/kotlin/com/moim/backend/domain/room/controller/RoomController.kt
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/v1/room")
+@RequestMapping("/api/v1/rooms")
 class RoomController(
     private val roomService: RoomService,
 ) {
