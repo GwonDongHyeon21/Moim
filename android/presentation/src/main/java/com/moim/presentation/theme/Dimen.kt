5cf4568bcbd5b193806a7937940a126f54216f7a
GwonDongHyeon21 <GwonDH21@gmail.com>
2026-07-10 22:55:45 +0900

    feat(and): 테마 Dimen 추가

---
+++ android/presentation/src/main/java/com/moim/presentation/theme/Dimen.kt
@@ -0,0 +1,29 @@
+package com.moim.presentation.theme
+
+import androidx.compose.ui.unit.dp
+
+object MoimPadding {
+    val AppHorizontalPadding = 16.dp
+
+    val PaddingXSmall = 8.dp
+    val PaddingSmall = 12.dp
+    val PaddingMedium = 16.dp
+    val PaddingLarge = 20.dp
+    val PaddingXLarge = 24.dp
+}
+
+object MoimSpace {
+    val SpaceXSmall = 8.dp
+    val SpaceSmall = 12.dp
+    val SpaceMedium = 16.dp
+    val SpaceLarge = 20.dp
+    val SpaceXLarge = 24.dp
+}
+
+object MoimIconSize {
+    val IconSizeXSmall = 8.dp
+    val IconSizeSmall = 16.dp
+    val IconSizeMedium = 24.dp
+    val IconSizeLarge = 32.dp
+    val IconSizeXLarge = 40.dp
+}
