3a3d799592656c51410bb8e2788e79c8c4d92b41
GwonDongHyeon21 <GwonDH21@gmail.com>
2026-07-27 22:50:23 +0900

    feat(and): UI용 후보지 모델 추가 및 더미 데이터 추가

---
+++ android/presentation/src/main/java/com/moim/presentation/screen/vote/model/CandidateUiModel.kt
@@ -0,0 +1,19 @@
+package com.moim.presentation.screen.vote.model
+
+import com.moim.domain.feature.vote.model.CandidateInfo
+
+data class CandidateUiModel(
+    val id: Long,
+    val category: String,
+    val content: String,
+    val creatorNickname: String,
+    val isVotedByMe: Boolean
+)
+
+fun CandidateInfo.toUiModel() = CandidateUiModel(
+    id = id,
+    category = category,
+    content = content,
+    creatorNickname = creatorNickname,
+    isVotedByMe = isVotedByMe
+)
--- android/presentation/src/main/java/com/moim/presentation/util/DummyData.kt
+++ android/presentation/src/main/java/com/moim/presentation/util/DummyData.kt
@@ -3,30 +3,50 @@ package com.moim.presentation.util
 import com.moim.presentation.model.RoomInfoUiModel
 import com.moim.presentation.screen.roomdetail.model.RoomDetailUiModel
 import com.moim.presentation.screen.roomdetail.model.RoomMemberInfoUiModel
+import com.moim.presentation.screen.vote.model.CandidateUiModel
 
 object DummyData {
 
     val dummyRooms = listOf(
         RoomInfoUiModel(
             code = "1",
-            title = "asdf",
-            description = "asdfasdf",
+            title = "테스트1",
+            description = "테스트 description1",
             maxCount = 10,
-            currentMemberCount = 4
+            currentMemberCount = 4,
+            deadline = "2026-05-15T14:30:00"
         ),
         RoomInfoUiModel(
             code = "2",
-            title = "qwer",
-            description = "qwerqwer",
+            title = "테스트2",
+            description = "테스트 description2",
             maxCount = 10,
-            currentMemberCount = 3
+            currentMemberCount = 3,
+            deadline = "2026-07-20T09:15:22"
         ),
         RoomInfoUiModel(
             code = "3",
-            title = "zxcv",
-            description = "zxcvzxcv",
+            title = "테스트3",
+            description = "테스트 description3",
+            maxCount = 100,
+            currentMemberCount = 10,
+            deadline = "2026-08-01T18:45:50"
+        ),
+        RoomInfoUiModel(
+            code = "4",
+            title = "테스트4",
+            description = "테스트 description4",
             maxCount = 10,
-            currentMemberCount = 10
+            currentMemberCount = 10,
+            deadline = "2026-08-01T18:45:50"
+        ),
+        RoomInfoUiModel(
+            code = "5",
+            title = "테스트5",
+            description = "테스트 description5",
+            maxCount = 15,
+            currentMemberCount = 8,
+            deadline = "2026-08-01T18:45:50"
         )
     )
 
@@ -49,5 +69,50 @@ object DummyData {
         roomInfo = dummyRooms.first(),
         role = "HOST",
         members = dummyMembers,
+    )
+
+    val dummyCandidates = listOf(
+        CandidateUiModel(
+            id = 1,
+            category = "FOOD",
+            content = "삼겹살",
+            creatorNickname = "nickname1",
+            isVotedByMe = true
+        ),
+        CandidateUiModel(
+            id = 2,
+            category = "FOOD",
+            content = "샤브샤브",
+            creatorNickname = "nickname2",
+            isVotedByMe = false
+        ),
+        CandidateUiModel(
+            id = 3,
+            category = "CAFE",
+            content = "메가커피",
+            creatorNickname = "nickname1",
+            isVotedByMe = true
+        ),
+        CandidateUiModel(
+            id = 4,
+            category = "CAFE",
+            content = "스타벅스",
+            creatorNickname = "nickname3",
+            isVotedByMe = false
+        ),
+        CandidateUiModel(
+            id = 5,
+            category = "PLACE",
+            content = "남산타워",
+            creatorNickname = "nickname1",
+            isVotedByMe = false
+        ),
+        CandidateUiModel(
+            id = 6,
+            category = "ACTIVITY",
+            content = "축구",
+            creatorNickname = "nickname4",
+            isVotedByMe = true
+        ),
     )
 }
