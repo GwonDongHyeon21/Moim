e374f888dfe46c64b609557cd7cd404fcf89ed2a
GwonDongHyeon21 <GwonDH21@gmail.com>
2026-07-27 21:14:56 +0900

    feat(and): 방 상세정보 화면 카테고리 카드, FAB 추가

--- android/presentation/src/main/java/com/moim/presentation/screen/roomdetail/RoomDetailScreen.kt
+++ android/presentation/src/main/java/com/moim/presentation/screen/roomdetail/RoomDetailScreen.kt
@@ -1,10 +1,17 @@
 package com.moim.presentation.screen.roomdetail
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.pulltorefresh.PullToRefreshBox
@@ -14,20 +21,30 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.moim.presentation.R
+import com.moim.presentation.model.Category
+import com.moim.presentation.model.RoomInfoUiModel
 import com.moim.presentation.navigation.RoomDetail
 import com.moim.presentation.screen.component.MoimProgressIndicator
 import com.moim.presentation.screen.component.MoimTopBar
+import com.moim.presentation.screen.roomdetail.component.CategoryCard
 import com.moim.presentation.screen.roomdetail.component.RoomCodeDialog
 import com.moim.presentation.screen.roomdetail.model.RoomDetailAction
 import com.moim.presentation.screen.roomdetail.model.RoomDetailEvent
 import com.moim.presentation.screen.roomdetail.model.RoomDetailUiState
+import com.moim.presentation.theme.MoimPadding
+import com.moim.presentation.theme.MoimSpace
 import com.moim.presentation.util.DummyData
 import com.moim.presentation.util.collectWithLifecycle
+import java.time.format.DateTimeFormatter
+import java.util.Locale
 
 @Composable
 fun RoomDetailScreen(
@@ -72,7 +89,7 @@ fun RoomDetailScreen(
     modifier: Modifier = Modifier
 ) {
     val roomInfo = uiState.roomDetail.roomInfo
-    var isExpanded by remember { mutableStateOf(false) }
+    var showRoomCode by remember { mutableStateOf(false) }
 
     val pullToRefreshState = rememberPullToRefreshState()
 
@@ -84,34 +101,68 @@ fun RoomDetailScreen(
                 navigationIcon = R.drawable.arrow_back_24,
                 actionIcon = R.drawable.more_vert_24,
                 onClickNavigationIcon = { onAction(RoomDetailAction.NavigateBack) },
-                onClickActionIcon = { isExpanded = true }
+                onClickActionIcon = { showRoomCode = true }
             )
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = { onAction(RoomDetailAction.NavigateToCandidateCreate) }) {
+                Icon(
+                    painter = painterResource(R.drawable.add_24),
+                    contentDescription = null
+                )
+            }
         }
     ) { innerPadding ->
         PullToRefreshBox(
             isRefreshing = uiState.isRefreshing,
             onRefresh = { onAction(RoomDetailAction.RefreshRoomDetail(roomInfo.id!!)) },
             state = pullToRefreshState,
-            modifier = Modifier.padding(innerPadding)
+            modifier = Modifier
+                .padding(innerPadding)
+                .padding(horizontal = MoimPadding.AppHorizontalPadding)
         ) {
             Column(
                 modifier = Modifier
                     .fillMaxSize()
-                    .verticalScroll(rememberScrollState())
+                    .verticalScroll(rememberScrollState()),
+                verticalArrangement = Arrangement.spacedBy(MoimSpace.SpaceSmall)
             ) {
-                Text(text = roomInfo.title)
-                Text(text = roomInfo.description.toString())
-                Text(text = roomInfo.currentMemberCount.toString())
-                Text(text = roomInfo.maxCount.toString())
+                RoomDetailInfoSection(roomInfo = roomInfo)
+
+                Category.entries.forEach { category ->
+                    CategoryCard(category.name) {
+                        onAction(RoomDetailAction.NavigateToVote(category.name))
+                    }
+                }
             }
         }
     }
 
-    if (isExpanded) {
+    if (showRoomCode) {
         RoomCodeDialog(
             roomCode = roomInfo.code,
-            onDismissRequest = { isExpanded = false }
+            onDismissRequest = { showRoomCode = false }
         )
+    }
+}
+
+@Composable
+fun RoomDetailInfoSection(roomInfo: RoomInfoUiModel) {
+    val uiFormatter =
+        DateTimeFormatter.ofPattern(stringResource(R.string.ui_time_format), Locale.KOREA)
+
+    Column {
+        Spacer(modifier = Modifier.height(MoimSpace.SpaceMedium))
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(text = roomInfo.description.toString())
+            Column(horizontalAlignment = Alignment.End) {
+                Text(text = roomInfo.deadline.format(uiFormatter))
+                Text(text = "${roomInfo.currentMemberCount} / ${roomInfo.maxCount}")
+            }
+        }
     }
 }
 
