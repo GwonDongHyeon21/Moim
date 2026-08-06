2cfdd6bb8d5c6a075de79aaa478e517bc4d26ba0
GwonDongHyeon21 <GwonDH21@gmail.com>
2026-08-06 22:43:07 +0900

    feat(back): 서버 시간대를 한국 시간대로 고정

--- backend/src/main/kotlin/com/moim/backend/BackendApplication.kt
+++ backend/src/main/kotlin/com/moim/backend/BackendApplication.kt
@@ -1,11 +1,19 @@
 package com.moim.backend
 
+import jakarta.annotation.PostConstruct
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import java.util.*
 
 @SpringBootApplication
-class BackendApplication
+class BackendApplication {
+
+    @PostConstruct
+    fun started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"))
+    }
+}
 
 fun main(args: Array<String>) {
-	runApplication<BackendApplication>(*args)
+    runApplication<BackendApplication>(*args)
 }
